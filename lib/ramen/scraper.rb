7a4320eaaeab05ddba6099e9045f30b93b4cebcc
Karen Flores <karen.ag.flores@gmail.com>
2018-11-18 23:32:18 +0000

    scraper dev

--- lib/ramen/scraper.rb
+++ lib/ramen/scraper.rb
@@ -3,11 +3,13 @@ require 'nokogiri'
 require 'pry'
 
 class Scraper
-  def self.scrape_page(url)
-    ramen_list = Nokogiri::HTML(open("https://sandiego.eater.com/maps/best-japanese-ramen-noodle-restaurant-san-diego"))
+  #url = "https://sandiego.eater.com/maps/best-japanese-ramen-noodle-restaurant-san-diego"
+  def self.scrape_page(url = "https://sandiego.eater.com/maps/best-japanese-ramen-noodle-restaurant-san-diego")
+    ramen_list = Nokogiri::HTML(open(url))
     ramen_spots = {}
     #ramen_list.css(".")
     
   end
   
 end
+Scraper.scrape_page(url)
