c315b78643e74498c719092ce2f8bb58b215d225
Karen Flores <karen.ag.flores@gmail.com>
2018-11-20 00:57:05 +0000

    fixing errors in Scraper

--- lib/ramen/scraper.rb
+++ lib/ramen/scraper.rb
@@ -7,8 +7,8 @@ class Scraper
   def self.scrape_page(url = "https://sandiego.eater.com/maps/best-japanese-ramen-noodle-restaurant-san-diego")
     ramen_list = Nokogiri::HTML(open(url))
     ramen_spots = {}
-    ramen_list.css(".c-mapstack__card").each do |ramen_shop|
-      binding.pry
+    ramen_list.css(".c-mapstack__cards").each do |ramen_shop|
+      #binding.pry
       ramen_spots = {
         :name => ramen_shop.css(".c-mapstack__card-hed").css("h1").text,
         :address => ramen_shop.css(".c-mapstack__address").text,
@@ -17,6 +17,7 @@ class Scraper
         #:website => ramen_shop
       }
     end
+    binding.pry
     ramen_spots
     
   end
