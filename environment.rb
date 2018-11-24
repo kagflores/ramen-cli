fc1ff6f60e212e402b130dc4ec40915acbb656b8
Karen Flores <karen.ag.flores@gmail.com>
2018-11-24 20:49:02 +0000

    got rid of unnecessary environment file

--- environment.rb
+++
@@ -1,8 +0,0 @@
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
-
-require_relative '../lib/ramen/scraper'
-require_relative '../lib/ramen/ramen'
-require_relative '../lib/ramen/cli'
-#require_relative '../lib/worlds_best_restaurants/version'
